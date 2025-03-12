ccd51930b5b0654d8cfbacbd76c9d567b038c146
sua <dssua0714@naver.com>
2025-03-12 14:18:58 +0900

    hello_world

--- pybo/views.py
+++ pybo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse(r"<h1>안녕하세요.</h1> <br> 게시판을 만들겠습니다.")
+
+
+# "http://127.0.0.1:8000/hello/
+def hello(request):
+    return HttpResponse("<h1>안녕하세요</h1>")
