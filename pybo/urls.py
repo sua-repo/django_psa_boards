4f6a00bc3a8824f911763910fbe6699808104105
sua <dssua0714@naver.com>
2025-03-17 10:30:49 +0900

    commit

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from pybo import views
 
+# http://127.0.0.1:8000/pybo 가 디폴트
 
 urlpatterns = [
     path("", views.index),
+    # http://127.0.0.1:8000/pybo/<int:question_id>
+    path("<int:question_id>/", views.detail),  # dev_3
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from pybo.models import Question
 
@@ -11,3 +11,15 @@ def index(request):
     question_list = Question.objects.order_by("-create_date")
     context = {"question_list": question_list}
     return render(request, "pybo/question_list.html", context)
+
+
+# http://127.0.0.1:8000/pybo/<int:question_id>/
+def detail(request, question_id):
+    # question = Question.objects.filter(id=question_id)[0]
+    # filter는 queryset을 반환하기 때문에 [0]을 붙여서 첫번째 데이터를 받아와야 한다.
+
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    context = {"question": question}
+    return render(request, "pybo/question_detail.html", context)
